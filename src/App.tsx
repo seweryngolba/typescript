62f059735fb6d90c3237f9c7d11d87c0bb984db7
Seweryn Golba <sgoolb@gmail.com>
2023-07-25 18:10:42 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Person, Country } from './Person';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+         <div className="App">
+      <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["Jessica", "Jake", "Jerry", "Thomas"]}
+        country={Country.Canada}
+      />
     </div>
+
   );
 }
 
