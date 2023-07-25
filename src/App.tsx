0bbf2c9ddbc1e78f79698a49a62075e881634a63
Seweryn Golba <sgoolb@gmail.com>
2023-07-25 18:55:20 +0200

    next commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
 import './App.css';
-import { Person, Country } from './Person';
+import { Provider } from "react-redux"
+import { store } from "./store";
+
 
 function App() {
   return (
 
-         <div className="App">
-      <Person
-        name="Pedro"
-        email="pedro@gmail.com"
-        age={21}
-        isMarried={true}
-        friends={["Jessica", "Jake", "Jerry", "Thomas"]}
-        country={Country.Canada}
-      />
+    <div className="App">
+      <Provider store={store}>
+<Router>
+  <Link to="/"> Home </Link>
+  <Link to="/login"> Login </Link>
+  <Link to="/contact"> Contact </Link>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+</Router>
+</Provider>
     </div>
 
   );
